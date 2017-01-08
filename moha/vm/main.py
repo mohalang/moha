29044a3c5440a0d94c716b9977e1c34de7d3d7c0
soasme <soasme@gmail.com>
2017-01-08 18:19:52 +1300

    migrate to new version of compiler.

--- moha/vm/main.py
+++ moha/vm/main.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from moha.vm.grammar import parse_source
+from moha.vm.grammar.v0_2_0 import parse_source
 from moha.vm.ast import transformer, CompilerContext
 from moha.vm.runtime import Frame, interpret_bytecode
+from moha.vm.compiler import Compiler
 
 def interpret_source(filename, source):
     bnf_node = parse_source(filename, source)
     if not bnf_node:
         return
-    ast = transformer.visit_main(bnf_node)
-    c = CompilerContext()
-    ast.compile(c)
-    bc = c.create_bytecode()
+    compiler = Compiler()
+    compiler.dispatch(bnf_node)
+    bc = compiler.create_bytecode()
+    #ast = transformer.visit_main(bnf_node)
+    #c = CompilerContext()
+    #ast.compile(c)
+    #bc = c.create_bytecode()
     frame = Frame(bc)
     interpret_bytecode(frame, bc)
